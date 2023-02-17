c9c318e8169a422560a295e664a6262e934449bf
dragontwosix <102781932+dragontwosix@users.noreply.github.com>
2023-02-17 17:47:18 -0500

    Update install.sh
    firefox-esr insead of brave
    add variety

--- install.sh
+++ install.sh
@@ -70,10 +70,8 @@ rm -rf Nordzy-cursors
 
 # Install brave-browser
 sudo nala install apt-transport-https curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo nala update
-sudo nala install brave-browser -y
+sudo nala install firefox-esr variety -y
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable sddm
